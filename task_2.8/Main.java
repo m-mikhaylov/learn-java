413b3ca0b4afdb3323372bd0f5870b91ccc9bd64
m-mikhaylov <74055567+m-mikhaylov@users.noreply.github.com>
2020-11-12 02:32:47 +0300

    Update Main.java

--- task_2.8/Main.java
+++ task_2.8/Main.java
@@ -4,7 +4,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int nums[] = {2,3,4,5};
+        int res = 1;
+        for (int i = 0; i < nums.length; i++) res *= nums[i];
+        System.out.println(res);
 
     }
 }
