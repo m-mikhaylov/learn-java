cde12879894d7b4be5b37d72e8d99c6550f09f44
m-mikhaylov <74055567+m-mikhaylov@users.noreply.github.com>
2020-11-12 01:39:31 +0300

    Update Main.java

--- task_2.12/Main.java
+++ task_2.12/Main.java
@@ -8,9 +8,33 @@
 3
 */
 
-public class Main {
-    public static void main(String[] args) {
-        //напишите тут ваш код
+	public static void main(String[] args) {
+	    
+	    int[] nums = {2, 4, 4, 4, 8, 8, 4, 12, 12, 14};
 
-    }
+		System.out.println(getMaxSeries(nums));
+	}
+	
+	public static int getMaxSeries(int[] nums)
+	{
+	    if (nums.length == 0) return 0;
+	    
+	    int res = 0;
+	    int current = nums[0];
+	    int currentSeries = 1;
+	    
+	    for (int i = 1; i < nums.length; i++) {
+	        if (nums[i] == current){
+	            currentSeries++;
+	        }
+	        else {
+	            if (currentSeries > res) res = currentSeries;
+	            current = nums[i];
+	            currentSeries = 1;
+	        }
+	    }
+	    return res;
+	    
+	}
+
 }
