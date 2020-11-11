f592c5d6407ff3e2e2de56a2699d2054f2e23c77
m-mikhaylov <74055567+m-mikhaylov@users.noreply.github.com>
2020-11-12 02:23:29 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -4,7 +4,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int nums[] = {1, -2, 13, 1};
+        int res = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > 10) {
+                res = i + 1;
+                break;
+            }
+        }
+        
+        System.out.println(res);
 
     }
 }
