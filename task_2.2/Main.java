38c2fe810ff98302c36d4fe763e5d00cc80c9174
m-mikhaylov <74055567+m-mikhaylov@users.noreply.github.com>
2020-11-12 02:46:35 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -6,7 +6,13 @@
 ... */
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
+        }
+        
 
     }
 }
