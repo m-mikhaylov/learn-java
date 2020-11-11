9095d37a9e3eb8454ed3670069e5b83194b3f13a
m-mikhaylov <74055567+m-mikhaylov@users.noreply.github.com>
2020-11-12 02:30:47 +0300

    Update Main.java

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -4,7 +4,21 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int nums[] = {1, -2, 13, 5, 1};
+        
+        boolean res = false;
+        
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == 5) {
+                res = true;
+                break;
+            }
+        }
+
+        String ress = "нет";
+        if (res) ress = "да";
+        System.out.println(ress);
 
     }
 }
